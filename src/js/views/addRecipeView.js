6a5c8411a3d1547830422516461b06b30e1b02ce
its-kat <khalsell83@gmail.com>
2023-08-03 21:09:31 -0700

    uploading recipe

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -3,6 +3,7 @@ import icons from 'url:../../img/icons.svg';
 
 class AddRecipeView extends View {
   _parentElement = document.querySelector('.upload');
+  _message = 'Recipe was successfully uploaded :)';
   _window = document.querySelector('.add-recipe-window ');
   _overlay = document.querySelector('.overlay');
   _btnOpen = document.querySelector('.nav__btn--add-recipe');
@@ -33,7 +34,9 @@ class AddRecipeView extends View {
       e.preventDefault();
 
       const dataArr = [...new FormData(this)];
+
       const data = Object.fromEntries(dataArr);
+
       handler(data);
     });
   }
