3e311f388d163879e7da27e376347e144ba6a2a0
its-kat <khalsell83@gmail.com>
2023-08-05 15:21:47 -0700

    removed fractional package and updated to fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,7 @@ import View from './View.js';
 
 // import icons from '../img/icons.svg'; //parcel 1
 import icons from 'url:../../img/icons.svg'; //parcel 2
-import { fraction } from 'fractional';
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -134,7 +134,7 @@ class RecipeView extends View {
         <use href="src/img/icons.svg#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
+        ing.quantity ? new fracty(ing.quantity).toString() : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
