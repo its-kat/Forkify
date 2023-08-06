9b4862f76e73faaaeacc2e8634e6fb010c9b79a7
its-kat <khalsell83@gmail.com>
2023-08-05 20:30:19 -0700

    cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -71,7 +71,6 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   //update the recipe view
-  // recipeView.render(model.state.recipe);
   recipeView.update(model.state.recipe);
 };
 
