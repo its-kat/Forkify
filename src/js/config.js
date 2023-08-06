5bbc1c1dc782bd6fb1eb6013d048514b7d6d6c3f
its-kat <khalsell83@gmail.com>
2023-08-05 19:50:23 -0700

    updated package json

--- src/js/config.js
+++ src/js/config.js
@@ -2,5 +2,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 5;
-export const API_KEY = '';
+export const API_KEY = '5c91f86b-90ed-4466-bc3e-ba3113d64986';
 export const MODAL_CLOSE_SEC = 2.5;
